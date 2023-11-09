2969eac902908fd50837dc3963c0dc0e7bef3d13
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2023-11-09 18:13:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,40 @@
 #! /usr/bin/bash
 
-dnf install blender btop wine krita freecad neofetch 
+#dnf install blender btop wine krita freecad neofetch 
 
-sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-sudo dnf install codium
+#sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+#printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+#sudo dnf install codium
 #for using arduino ide, you have to give the usb port permission
-ls -l /dev/ttyUSB*
-sudo usermod -a -G dialout <username>
+#ls -l /dev/ttyUSB*
+#sudo usermod -a -G dialout <username>
 
+## install script ## 
+
+#!/bin/bash
+
+# Check if the script is run with root privileges
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run with sudo or as root."
+  exit 1
+fi
+
+# Install neofetch and lolcat
+dnf install -y neofetch lolcat
+
+# Check if neofetch installation was successful
+if [ $? -ne 0 ]; then
+  echo "Neofetch installation failed. Exiting."
+  exit 1
+fi
+
+# Configure bashrc to display neofetch with lolcat
+echo -e "\n# Display Neofetch with Lolcat\nneofetch | lolcat" >> /etc/bashrc
+
+# Inform the user about successful installation and configuration
+echo "Neofetch and Lolcat installed successfully. Bashrc configured."
+
+# Source the bashrc to apply changes immediately
+source /etc/bashrc
+
+exit 0
