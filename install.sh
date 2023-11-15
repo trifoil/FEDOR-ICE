b4d2ef6b6ebd068aa1997aedf9aabf32ee72fff1
trifoil <unixsslayer@gmail.com>
2023-11-15 01:49:40 +0100

    removed freecad

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Install neofetch and lolcat
-sudo dnf install -y neofetch lolcat btop blender wine krita freecad 
+sudo dnf install -y neofetch lolcat btop blender wine krita  
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-sudo dnf install codium
+sudo dnf install codium -y
 
-sudo dnf copr enable bgstack15/AfterMozilla
-sudo dnf install librewolf
+sudo dnf copr enable bgstack15/AfterMozilla -y
+sudo dnf install librewolf -y
 
-sudo dnf install dnf-plugins-core
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser
+sudo dnf install dnf-plugins-core -y
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -y
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc -y
+sudo dnf install brave-browser -y
 
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf install vlc
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+dnf install vlc -y
 
 # Check if neofetch installation was successful
 if [ $? -ne 0 ]; then
